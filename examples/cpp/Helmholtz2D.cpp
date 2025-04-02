b6f9b6626e8e93a3ef20692342aa3195ca434747
Hunti0 <143464115+Hunti0@users.noreply.github.com>
2025-04-02 15:54:46 -0700

    Update Helmholtz2D.cpp

--- examples/cpp/Helmholtz2D.cpp
+++ examples/cpp/Helmholtz2D.cpp
@@ -4,21 +4,22 @@
 #include "mole.h" 
 int main() {
     
-using namespace std;
-using namespace Eigen;
-
 const double aa = 0.7;   // Wall reflection coefficient
 const double bb = 0.9 * 0.5;  // Wall absorption coefficient
 const double wn = 6.0;   // Angular wave number
 
+ //Spatial Discretization
 const int k = 2;  // Order of accuracy
 const int m = 500, n = 500;  // Grid resolution
 const double a = 0, b = 40;  // X domain
 const double c = 0, d = 40;  // Y domain
-const double dx = (b - a) / m, dy = (d - c) / n;  // Grid spacing
+const double dx = (b - a) / m; // Grid spacing
+const double dy = (d - c) / n;  // Grid spacing
 
 // Hotspot location
-const double hsx = 2.0, hsy = 10.0, hsr = 1.0;
+const double hsx = 2.0;
+const double hsy = 10.0;
+const double hsr = 1.0;
 
 bool isWall(double x, double y) {
     return ((x >= 10.0 && x <= 39.0 && y >= 20.0 && y <= 21.0) ||
