ee8b8254e452884755519cec72e00ec0ec4fb121
Hunti0 <143464115+Hunti0@users.noreply.github.com>
2025-04-02 15:13:20 -0700

    Update Hyperbolic1D.cpp

--- examples/cpp/Hyperbolic1D.cpp
+++ examples/cpp/Hyperbolic1D.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "mole.h"
-#include <math.h>
+#include <vector>
+#include <cmath>
 int main() {
     double a = 1; //Velocity
     double west = 0; // Left domain limit
@@ -15,9 +16,23 @@ int main() {
 
     Div = D(k, m, dx);
     Inter = I(m, 0.5);
+     //grid 
+    BC1D periodicBC(k, m, 1, 1, 0);
 
-    //vec grid()
+     mat D = -a * dt * 2  * Div * Inter
 
-    U = sin(2*pi*) //IC
+     vector<double> U(m), U2(m), U3(m);
+    for (int i = 0; i < m; ++i) {
+        double x = grid.x(i);
+        U[i] = sin(2 * M_PI * x); // Initial condition
+    }
+
+    U2 = U + 0.5 * (Div * U)
+
+    for (double t = dt; t<= t_final; t += dt) {
+        U3 = U + (D * U2);
+        U = U2;
+        U2 = U3;
+        
     return 0;
 }
